9b8d683c0ff0013873feac3b43a2ce8ec91232e4
Chen Hua <chua0221@gmail.com>
2023-10-07 22:23:35 -0700

    add to remote repo

---
+++ cmake-build-debug/CMakeFiles/leetcodecpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcodecpp.dir/main.cpp.o"
+  "leetcodecpp"
+  "leetcodecpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcodecpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
